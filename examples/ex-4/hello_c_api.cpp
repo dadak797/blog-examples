89ed31dba011b5cbaf0ab13dac0ef8998552c2b6
dadak797 <dadak797@gmail.com>
2026-08-14 21:18:24 +0900

    feat: add C++-to-JavaScript return value examples

--- examples/ex-4/hello_c_api.cpp
+++ examples/ex-4/hello_c_api.cpp
@@ -16,4 +16,22 @@ extern "C" {
   void hello_with_name(const char* name) {
     std::cout << "Hello, " << name << "! (From C++)" << std::endl;
   }
+
+  EMSCRIPTEN_KEEPALIVE
+  const char* get_hello() {
+    return "Hello from C++!";
+  }
+
+  struct MyString {
+    const char* str;
+    size_t length;
+  };
+
+  EMSCRIPTEN_KEEPALIVE
+  MyString get_hello_struct() {
+    MyString myStr;
+    myStr.str = "Hello from C++ struct!";
+    myStr.length = strlen(myStr.str);
+    return myStr;
+  }
 }
