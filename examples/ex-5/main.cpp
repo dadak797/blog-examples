5745861d1a7a99ff4c220711541a2b1094bced33
dadak797 <dadak797@gmail.com>
2026-08-14 21:33:06 +0900

    feat: demonstrate JavaScript calls from C++ using EM_ASM

--- examples/ex-5/main.cpp
+++ examples/ex-5/main.cpp
@@ -3,20 +3,20 @@
 
 int main() {
   EM_ASM({
-    console.log("Hello, WebAssembly! (From C++)");
+    console.log("Hello, WebAssembly! (From C++ via JavaScript)");
   });
 
   int c_number = 100;
   EM_ASM({
     const js_number = $0; 
-    console.log("Hello, " + js_number + "! (From C++ with number)");
+    console.log("Hello, " + js_number + "! (From C++ with number via JavaScript)");
   }, c_number);
 
   const char* c_name = "WebAssembly";
   EM_ASM({
     const js_name_ptr = $0;
     const js_name = UTF8ToString(js_name_ptr);
-    console.log("Hello, " + js_name + "! (From C++ with string)");
+    console.log("Hello, " + js_name + "! (From C++ with string via JavaScript)");
   }, c_name);
 
   return 0;
